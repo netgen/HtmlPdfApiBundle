632f66f4e216d914985253bebfa42f91e9957970
Ivan Herak <ivan@netgen.hr>
2014-06-24 03:22:42 +0200

    Clean up

--- DependencyInjection/NetgenHtmlPdfApiExtension.php
+++ DependencyInjection/NetgenHtmlPdfApiExtension.php
@@ -9,8 +9,6 @@ use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
 class NetgenHtmlPdfApiExtension extends Extension
 {
